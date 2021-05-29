8e435dea622a089fc3a5d57950f309dce26830c3
woojiahao <woojiahao1234@gmail.com>
2021-05-29 21:38:10 +0800

    Add config for dev API path

--- config/dev.exs
+++ config/dev.exs
@@ -65,3 +65,5 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :hearth, api_path: "localhost:4001"
