1e5dc7e4a3d759f8657e3afa3cfc9387c757e070
woojiahao <woojiahao1234@gmail.com>
2021-05-29 21:53:50 +0800

    Use URI to merge URLs

--- lib/hearth/kobold_url.ex
+++ lib/hearth/kobold_url.ex
@@ -1,16 +1,21 @@
 defmodule Hearth.KoboldUrl do
   # TODO: Assess if this needs to be a GenServer
+  @moduledoc """
+  This module is a wrapper around the kobold URL service. In order to simplify the work, HTTPoison.Base is used where
+  callbacks are defined to help with standardising headers and URLs. The requests are also simplified by curating how
+  the processing of requests after they have been passed through the various callbacks
+  """
   use HTTPoison.Base
 
   def shorten(url) do
     # TODO: Allow other options to be used but tie them to a user account like expiration date using dashboard to control
-    case request_(:post, "create", %{"original" => url}) do
+    case post_("create", %{"original" => url}) do
       {:ok, body} -> {:ok, body["message"]}
       {:error, error} -> {:error, error}
     end
   end
 
-  def request_(:post, url, body) do
+  def post_(url, body) do
     case post(url, body) do
       {:ok, %HTTPoison.Response{status_code: code, body: body}} when code in 200..299 ->
         {:ok, body}
@@ -27,7 +32,9 @@ defmodule Hearth.KoboldUrl do
   def process_url(url) do
     base = Application.get_env(:hearth, :api_path)
     # TODO: Properly generate URL
-    base <> "/" <> url
+    base
+    |> URI.merge(url)
+    |> URI.to_string()
   end
 
   def process_request_headers(headers) do
