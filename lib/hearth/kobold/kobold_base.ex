23a2dd4d7c26fbc74622b81f98b1f022eefc9094
woojiahao <woojiahao1234@gmail.com>
2021-06-03 20:51:08 +0800

    Move any modified http func to macro body

--- lib/hearth/kobold/kobold_base.ex
+++ lib/hearth/kobold/kobold_base.ex
@@ -1,15 +1,9 @@
 defmodule Hearth.KoboldBase do
-  import HTTPoison
-
   defmacro __using__(opts) do
     quote location: :keep, bind_quoted: [opts: opts] do
       use HTTPoison.Base
 
-      import Hearth.KoboldBase
-
       url_func = Keyword.get(opts, :url_func)
-
-      def process_url(url), do: unquote(url_func).(url)
 
       def process_request_headers(headers) do
         h = [
@@ -20,22 +14,25 @@ defmodule Hearth.KoboldBase do
         headers ++ h
       end
 
+      def process_url(url), do: unquote(url_func).(url)
       def process_request_body(body), do: Jason.encode!(body)
       def process_response_body(body), do: Jason.decode!(body)
-    end
-  end
 
-  def post?(url, body) do
-    case post(url, body) do
-      {:ok, %HTTPoison.Response{status_code: code, body: body}} when code in 200..299 ->
-        {:ok, body}
+      def post?(url, body) do
+        case post(url, body) do
+          {:ok, %HTTPoison.Response{status_code: code, body: body}} when code in 200..299 ->
+            {:ok, body}
 
-      {:ok, %HTTPoison.Response{body: body}} ->
-        error = body |> Map.get("error", body |> Map.get("errors", ""))
-        {:error, error}
+          {:ok, %HTTPoison.Response{body: body}} ->
+            IO.inspect(body)
+            error = body |> Map.get("error", body |> Map.get("errors", ""))
+            {:error, error}
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+          {:error, %HTTPoison.Error{reason: reason}} ->
+            IO.inspect("reason #{reason}")
+            {:error, reason}
+        end
+      end
     end
   end
 end
