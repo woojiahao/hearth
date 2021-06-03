87d179f47d07ddf0129dccebb5e5702fde3faedc
woojiahao <woojiahao1234@gmail.com>
2021-06-03 19:39:31 +0800

    Add login func for auth wrapper

--- lib/hearth/kobold/kobold_auth.ex
+++ lib/hearth/kobold/kobold_auth.ex
@@ -1,6 +1,16 @@
 defmodule Hearth.KoboldAuth do
   use Hearth.KoboldBase, url_func: &Hearth.create_auth_url/1
 
-  def login(username, password) do
+  @type email :: String.t()
+  @type password :: String.t()
+  @type access_token :: String.t()
+  @type refresh_token :: String.t()
+
+  @spec login(email, password) :: {:ok, access_token, refresh_token} | {:error, String.t()}
+  def login(email, password) do
+    case post?("auth/login", %{"email" => email, "password" => password}) do
+      {:ok, body} -> {:ok, body["access_token"], body["refresh_token"]}
+      {:error, error} -> {:error, error}
+    end
   end
 end
